3f056476bfc97188cc2887f94ee2437157fc21ab
rmullett <rdmullett@gmail.com>
2019-11-22 12:57:03 -0700

    add podman-static.py, which creates a very basic container with hardcoded values

---
+++ podman-static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""This example will create a basic fedora container that is made with hardcoded options, and delete it upon exit."""
+
+import podman
+
+client = podman.Client()
+pull = client.images.pull("fedora:latest")
+image = client.images.get(pull)
+opts = {
+    'memory': '1G',
+    'memory-reservation': '750M',
+    'Memory-swap': '1.5G',
+    }
+container = image.create(detach=True, tty=True, command=['/bin/bash'])
+container.attach(eot=4)
+try:
+    container.start()
+    print()
+except (BrokenPipeError, KeyboardInterrupt):
+    print('\nContainer disconnected.')
+    container.remove(force=True)
